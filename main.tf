fa4b4c9e16eee2931e183a5bdb2b5aedfc425597
apoorvabh98 <apoorvabh98@gmail.com>
2026-05-20 01:25:19 +0530

    Initial GKE terraform code

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-73596eb5-7470-44c4-beb"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "free-tier-gke"
+  location = "us-central1-a"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "node-pool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.primary.name
+
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-micro"
+    disk_size_gb = 20
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
